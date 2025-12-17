b780e680f5136d9342883b86132f975561b602e2
saipavan2143 <ssaipavan2003@gmail.com>
2025-12-17 12:08:47 +0530

    adding again

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ b = 5
 print(a + b)
 print("Hello, World!")
 print("This is a sample Python script.")
+#logic to calculate the sum of two numbers
+num1 = 10   
+num2 = 20
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum)
+# End of the script
